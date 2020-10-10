6d988ba65f9da3921a5d90bed10b94760f9a83db
两支万宝路 <james_bond1@163.com>
2020-10-11 00:21:34 +0800

    2020/10/11

---
+++ 换硬币.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int dp[100005];
+int maxn = 200000;
+
+int coinChange(vector<int> &coins, int amount) {
+    dp[0] = 0;
+    for (int i = 1; i <= amount; i++) {
+        dp[i] = maxn;
+        for (int j = 0; j < coins.size(); j++) {
+            if (i - coins[j] >= 0)
+                dp[i] = min(dp[i - coins[j]] + 1, dp[i]);
+        }
+    }
+    if (dp[amount] == maxn) {
+        return -1;
+    } else {
+        return dp[amount];
+    }
+}
+
+int main() {
+    int n;
+    int num, amount;
+    vector<int> coins;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> num;
+        coins.emplace_back(num);
+    }
+    cin >> amount;
+    coinChange(coins, amount);
+    return 0;
+}
