29f3ae5413f5bfb9d8c5a557ba408403716ae8ce
Falcone-H <james_bond1@163.com>
2020-10-27 23:53:52 +0800

    2020/10/27

---
+++ 完全背包.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+#define int long long
+using namespace std;
+int n, m;
+int w[105], v[105];
+int dp[105][10005];
+
+signed main() {
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        cin >> w[i] >> v[i];
+    }
+    for (int i = 1; i <= n; i++) {
+        for (int j = w[i]; j <= m; j++) {
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - w[i]] + v[i]);
+        }
+    }
+    return 0;
+}
+
+/*
+空间优化:
+for(int i = 1; i <= n; i++){
+    for(int j = w[i]; j <= m; j++){     //必须正序枚举,更新前面的状态
+        dp[j] = max(dp[j], dp[j - w[i]] + v[i]);
+    }
+}
+*/
