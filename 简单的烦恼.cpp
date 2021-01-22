18d8a81859c80b33b5f90163ea1d78506eb80218
Falcone-H <james_bond1@163.com>
2021-01-22 23:33:32 +0800

    2021/1/22

---
+++ 简单的烦恼.cpp
@@ -0,0 +1,30 @@
+// https://ac.nowcoder.com/acm/problem/25184
+// 思路：01背包
+// 每首歌都可以听无数次，然后相当于在以time - 1为容量的背包中，挑选物品，每一种物品都有无数个。
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    int a[1000];
+    int dp[80005];
+    while (t--) {
+        int n, time;
+        memset(a, 0, sizeof(a));
+        memset(dp, 0, sizeof(dp));
+        cin >> n >> time;
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        sort(a, a + n);
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = time - 1; j >= a[i]; j--) {
+                dp[j] = max(dp[j], dp[j - a[i]] + a[i]);
+            }
+        }
+        cout << dp[time - 1] + a[n - 1] << endl;
+    }
+    return 0;
+}
