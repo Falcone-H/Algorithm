7e236c2d8fb97930de618b383d01b349ca67702a
Falcone-H <james_bond1@163.com>
2021-02-27 13:52:40 +0800

    2021/2/27

---
+++ 拓展欧几里得求逆元.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+int exgcd(int a, int b, int &x, int &y)
+{
+    if (b == 0)
+    {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    int d = exgcd(b, a % b, y, x);
+    y -= a / b * x;
+    return d;
+}
+signed main()
+{
+    int a, b;
+    cin >> a >> b; // 求 a 关于 b 的逆元，ax + by = 1，即 ax + by = gcd(a, b);  a, b互质
+    int x = 0;
+    int y = 0;
+    exgcd(a, b, x, y);
+    cout << (x % b + b) % b << endl; // 防止出现负数
+    return 0;
+}
