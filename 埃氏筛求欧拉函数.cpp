aad1fd5d9f81319e5b26cc0f041c3bed5aada172
Falcone-H <james_bond1@163.com>
2021-02-27 20:53:07 +0800

    2021/2/27

--- 埃氏筛求欧拉函数.cpp
+++ 埃氏筛求欧拉函数.cpp
@@ -1,21 +1,27 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define int long long
 using namespace std;
 int phi[100000];
-void euler(int n) {
-    for(int i = 1; i <= n; i++)
+void euler(int n)
+{
+    for (int i = 1; i <= n; i++)
         phi[i] = i;
-    for(int i = 2; i <= n; i++) {
-        if(phi[i] == i) {   // 代表 i 是质数
-            for(int j = i; j <= n; j+= i) {
-                phi[j] = phi[j] / i * (i - 1);  // 把 i 的倍数更新掉
+    for (int i = 2; i <= n; i++)
+    {
+        if (phi[i] == i)
+        { // 代表 i 是质数
+            for (int j = i; j <= n; j += i)
+            {
+                phi[j] = phi[j] / i * (i - 1); // 把 i 的倍数更新掉
             }
         }
     }
 }
-signed main() {
+signed main()
+{
     euler(8);
-    for(int i = 1; i <= 8; i++) {
+    for (int i = 1; i <= 8; i++)
+    {
         cout << phi[i] << endl;
     }
     return 0;
